992266b3e7799b53345e594803fb99775b50eba3
gabriel <nextzyagency@gmail.com>
2026-03-29 11:11:55 -0500

    chore: update site and base for nextzyagency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://gabo0.github.io', // Placeholder based on system path
-  base: '/pagina-nextzy',             // Based on folder name
+  site: 'https://nextzyagency.github.io', 
+  base: '/nextzyagency',             
   integrations: [tailwind()],
 });
