985bc86025bf9514ed54591b5d26066724d4024a
gabriel <nextzyagency@gmail.com>
2026-04-11 12:55:54 -0500

    fix css and js

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://nextzyagency.github.io', 
+  site: 'https://nextzyagency.online', 
   base: '/',             
   integrations: [tailwind()],
 });
